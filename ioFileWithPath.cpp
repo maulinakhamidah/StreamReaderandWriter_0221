2a3db15976d57f71b1e60cf4c6dc62ea18464d98
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-06-09 18:31:23 +0700

    mengembalikan nilai

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -25,5 +25,9 @@ int main()
         // mendapatkan setiap karakter dalam satu baris
         getline(cin, baris);
         // loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q")
+        break;
+        // menulis dan memasukan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
     }
 }
