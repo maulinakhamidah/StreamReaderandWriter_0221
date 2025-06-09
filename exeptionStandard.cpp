a25cd979910bbde5a929074b95f227c4354c9754
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-06-09 19:02:53 +0700

    membuka file untuk menulis

---
+++ exeptionStandard.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <exception>
+// untuk obyek exception yanga akan digunakan 
+#include <array>
+// untuk obyek array yanga akan kita guanakan
+using namespace std;
+
+int main()
+{
+    cout << "Awal Program" << endl; // penanda 1:...
+    try
+    {
+        array<int, 3> data = {10, 20, 30};
+        // pesan array integer 3 elemen
+        cout << data.at(4) << endl;
+        // memanggil array elemen 5
+    }
+    catch (exception& e)
+    {
+        // penangkap menggunakan obyek exeception
+        cout << e.what() << endl;
+        /*akan dieksekusi karena array data hanya memiliki 3 elemen*/
+       
+    }
+    cout << "Baris Program Yang terakhir" << endl;
+    /*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+
+    return 0;
+    
+}
+
--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -11,4 +11,11 @@ int main()
     cout << " Masukan Nama file : ";
     cin >> NamaFile;
 
+    // membuka file dalam mode menulis
+    ofstream outfile;
+    // menujuk ke sebuauh nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
     
