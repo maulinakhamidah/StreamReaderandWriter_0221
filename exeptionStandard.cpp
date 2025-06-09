c04359383bebbf0eb06b642c96b639a19dc81767
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-06-09 21:39:17 +0700

    menangkap exception yang dilempar dan mengecek error

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -14,5 +14,12 @@ int main()
         // pesan array integer 3 elemen
         cout << data.at(4) << endl;
         // memanggil array elemen 5
-  
+    }
+    catch (exception& e)
+    {
+        // penangkap menggunakan obyek exeception
+        cout << e.what() << endl;
+        /*akan dieksekusi karena array data hanya memiliki 3 elemen*/
+       
+ 
 
