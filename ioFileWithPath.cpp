134b4d6349c2d1dedbce79d98364686fc172db55
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-06-09 18:39:46 +0700

    membuat header dan namespace

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -3,31 +3,3 @@
 #include <string>
 using namespace std;
 
-int main()
-{
-    string baris;
-    string NamaFile;
-
-    cout << " Masukan Nama file : ";
-    cin >> NamaFile;
-
-    // membuka file dalam mode menulis
-    ofstream outfile;
-    // menujuk ke sebuauh nama file
-    outfile.open(NamaFile + ".txt", ios::out);
-
-    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
-
-    // unlimited loop untuk menulis
-    while (true)
-    {
-        cout << "-";
-        // mendapatkan setiap karakter dalam satu baris
-        getline(cin, baris);
-        // loop akan berhenti jika anda memasukan karakter q
-        if (baris == "q")
-        break;
-        // menulis dan memasukan nilai dari 'baris' ke dalam file
-        outfile << baris << endl;
-    }
-}
