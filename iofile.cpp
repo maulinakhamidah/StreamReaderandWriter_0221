74ab99f9d89a33ab6775122e1872b56b5aa205bc
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-06-09 18:17:47 +0700

    selesai dalam menulis sekarang tutup

--- iofile.cpp
+++ iofile.cpp
@@ -16,4 +16,18 @@ int main()
 
     // unlimited loop untuk menulis
     while (true)
-    
+    {
+        cout << "-";
+        // mendapatkan setiap karakter dalam satu baris
+        getline(cin,baris);
+        // loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q")
+        break;
+        // menulis dan memasukan nilai 'baris' ke dalam file
+        outfile << baris <<endl;
+    }
+    // selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+
+}
