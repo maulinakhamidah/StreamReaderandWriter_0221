e02777c21775eca069eb594b1be7d118b313d771
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-06-09 21:33:29 +0700

    mencoba mengakses elemen ke-5

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -5,5 +5,14 @@
 // untuk obyek array yanga akan kita guanakan
 using namespace std;
 
-
+int main()
+{
+    cout << "Awal Program" << endl; // penanda 1:...
+    try
+    {
+        array<int, 3> data = {10, 20, 30};
+        // pesan array integer 3 elemen
+        cout << data.at(4) << endl;
+        // memanggil array elemen 5
+  
 
