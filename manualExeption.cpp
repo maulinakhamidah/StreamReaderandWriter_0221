72ad0210e2a267f71ab17c145bb8b6319e96afab
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-06-09 19:10:01 +0700

    melemparkan sebuah integer

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -6,3 +6,8 @@ using namespace std;
 int main()
 {
     try
+    {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw("halo"); //meleparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+   
