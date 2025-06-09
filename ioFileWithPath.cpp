6c517bdd74bc76018a12f2d032dafdef52b46bbd
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-06-09 18:27:01 +0700

    mencetak output kelayar

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -5,5 +5,25 @@ using namespace std;
 
 int main()
 {
-    
+    string baris;
+    string NamaFile;
+
+    cout << " Masukan Nama file : ";
+    cin >> NamaFile;
+
+    // membuka file dalam mode menulis
+    ofstream outfile;
+    // menujuk ke sebuauh nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    // unlimited loop untuk menulis
+    while (true)
+    {
+        cout << "-";
+        // mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        // loop akan berhenti jika anda memasukan karakter q
+    }
 }
